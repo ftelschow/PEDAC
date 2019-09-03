a5c25eac4711796662c15c1c463c1bf0588bdaee
Fabian Telschow <ftelschow@ucsd.edu>
2019-09-03 20:45:39 +0200

    added option for variable rho

--- generate_AR.m
+++ generate_AR.m
@@ -1,11 +1,22 @@
-function X = generate_AR(Msim, N, rho, sigma)
+function X = generate_AR(Msim, N, rho, sigma, rho_se)
+  if nargin < 5
+      rho_se = 0;
+  end
+  
+  if rho_se ~= 0
+      rho1 = max(rho + normrnd(0, rho_se, [1,Msim]), 0);
+  else
+      rho1 = repmat(rho, [1, Msim]);
+  end
+  
   % Generate AR process with AR parameter rho and marginal variance 1
   X = zeros([N,Msim]);
   for m = 1:Msim
       eps = normrnd(0,1,[1,N]);
+      
       X(1,m) = eps(1);
       for i = 2:N
-          X(i,m) = rho*X(i-1,m) + sqrt(1-rho^2)*eps(i);
+          X(i,m) = rho1(m)*X(i-1,m) + sqrt(1-rho1(m)^2)*eps(i);
       end
   end
 X = sigma*X;

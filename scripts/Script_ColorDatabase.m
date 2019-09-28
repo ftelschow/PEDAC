85b1bcda30fd96d51cb8c1970d8ab84daafe98d6
Fabian Telschow <ftelschow@ucsd.edu>
2019-09-28 09:44:26 +0200

    add script saving useful color schemes for color blind people and greyscale printing

---
+++ scripts/Script_ColorDatabase.m
@@ -0,0 +1,36 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%
+%%%%	This script produces a color data base for color blind and grey
+%%%%    scale compatible figures
+%%%%
+%%%%    Output: colors.mat
+%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% clear workspace
+clear all
+close all
+
+% set correct working directory
+path_data = '/home/drtea/Research/Projects/CO2policy/PEDAC/data/';
+%%%% standard color scheme for color blind and grey scale figures, cf.
+%%%% 'https://personal.sron.nl/~pault/'
+BrightCol  = [[68 119 170];...    % blue
+              [102 204 238];...   % cyan
+              [34 136 51];...     % green
+              [204 187 68];...    % yellow
+              [238 102 119];...   % red
+              [170 51 119];...    % purple
+              [187 187 187]]/255; % grey
+          
+Vibrant    = [[0 119 187];... % blue
+              [51 187 238];...% cyan
+              [0 153 136];... % teal
+              [238 119 51];...% orange
+              [204 51 17];... % red
+              [238 51 119];...% magenta
+              [187 187 187]...% grey
+              ]/255;
+
+colMat = Vibrant([1 3 4 5],:);
+
+save(strcat(path_data,'colors.mat'), 'BrightCol', 'Vibrant', 'colMat')

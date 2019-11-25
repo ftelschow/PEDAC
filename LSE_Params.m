4c0e7cff54f8c9d5b6593b7b2349dd88098ad0f1
Fabian Telschow <ftelschow@ucsd.edu>
2019-11-25 09:33:32 +0100

    minor changes

--- LSE_Params.m
+++ LSE_Params.m
@@ -1,10 +1,13 @@
 % Least squares loss between Joos model and observation
 function loss = LSE_Params( x, data, obs, start_year, end_year )
   % calculate Joosmodel for some parameter x and past emission data
-  tmp     = JoosModelFix( data, x);
-  % differenz in indizes because of starting time.
-  dI      = size(data,1)-size(obs,1);
+  tmp     = JoosModel( data, x);
+  
   % find the indices for the year we define the loss on
-  Ia      = find( data(:,1) == start_year );
-  Ie      = find( data(:,1) == end_year );
-  loss = mean( (tmp(Ia:Ie,2) - obs((Ia-dI):(Ie-dI),2)).^2 );
+  I1a      = find( tmp(:,1) == start_year );
+  I1e      = find( tmp(:,1) == end_year );
+  I2a      = find( obs(:,1) == start_year );
+  I2e      = find( obs(:,1) == end_year );
+  
+  % compute the loss as non weighted mean square
+  loss = mean( ( tmp(I1a:I1e,2) - obs(I2a:I2e,2) ).^2 );
--- findBase.m
+++ findBase.m
@@ -1,30 +1,14 @@
-function [val type] = findBase(A, B, C)
+function val = findBase( A, B )
     i = 1;
-    found = 0;
-    while i <=size(B,1) && found == 0
+    val = 0;
+    while i <=size(B,1) && val == 0
         if( strcmp(A{1},B{i,1}) && strcmp(A{2},B{i,2}) )
-            found = i;
+            val = i;
         else
             i = i+1;
         end
     end
-    if(~strcmp("",B{found,4}))
-        i = 1;
-        val = 0;
-        while i <=size(C,1) && val == 0
-            if( strcmp(C{i,1},B{found,1}) && strcmp(C{i,2},B{found,4}) )
-                val = i;
-            else
-                i = i+1;
-            end
-        end
-        type = B{found,3};
-    else
-        val  = NaN;
-        type = '';
-    end
     if val == 0
         val  = NaN;
-        type = '';
     end
 end

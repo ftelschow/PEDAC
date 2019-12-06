982c1ab6b79a5df48345bddd8725b3e8490cff15
Fabian Telschow <ftelschow@ucsd.edu>
2019-12-06 13:46:12 +0100

    styling

--- get_Thresholds.m
+++ get_Thresholds.m
@@ -1,4 +1,4 @@
-function thresholds = get_Thresholds(process, q, method)
+function thresholds = get_Thresholds( process, q, method )
 switch nargin
     case 1
         q = 0.05;
@@ -7,38 +7,37 @@ switch nargin
         method = 'minimum';
 end
 
-N = size(process,2);
+N = size( process, 2 );
 
 % allocate the threshold vector
-T = size(process,1);
-thresholds = ones([1 T])*NaN;
+T = size( process, 1 );
+thresholds = ones( [ 1 T ] ) * NaN;
 
-
-if strcmp(method, 'minimum')
+if strcmp( method, 'minimum' )
   % Get cummulative probability of detecting an exeedance for all
   % thresholds
   for t = 1:T
-    if t==1
-        mProc = process(1,:);
+    if t == 1
+        mProc = process( 1, : );
     else
-        mProc = min(process(1:t,:));
+        mProc = min( process( 1:t, : ) );
     end
-    thresholds(t)  = quantile(mProc, q);
+    thresholds( t )  = quantile( mProc, q );
   end
 else
   % Get cummulative probability of detecting an exeedance for all
   % thresholds
   for t = 1:T
-    if t==1
-        mProc = process(1,:);
+    if t == 1
+        mProc = process( 1, : );
     else
-        v      = ones([t N])*NaN;
-        v(1,:) = process(1,:);
+        v         = ones( [ t N ] ) * NaN;
+        v( 1, : ) = process( 1, : );
         for tt = 2:t
-            v(tt,:) = mean(process(1:tt,:));
+            v( tt, : ) = mean( process( 1:tt, : ) );
         end
-        mProc = min(v);
+        mProc = min( v );
     end
-    thresholds(t)  = quantile(mProc, q);
+    thresholds( t ) = quantile( mProc, q );
   end   
 end

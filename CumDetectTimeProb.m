b94db15fa9ca35a5ef950a577024dd9b1a1c94b8
Fabian Telschow <ftelschow@ucsd.edu>
2019-12-06 13:35:38 +0100

    remove not needed file

--- CumDetectTimeProb.m
+++
@@ -1,7 +0,0 @@
-function CumSumProb = CumDetectTimeProb(processMat, thresh)
-  % Probability that detection time is greater than t
-  detect = processMat;
-  for j = 1:size(detect,2)
-    detect(:,j) = cumsum(processMat(:,j) < thresh) > 0;
-  end
-  CumSumProb = mean(detect,2);

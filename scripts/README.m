2342982805490db1cae1f9f6864f448f964b33ad
Fabian Telschow <ftelschow@ucsd.edu>
2019-12-09 16:01:32 +0100

    modified to work witht he IIASA data

--- scripts/README.m
+++ scripts/README.m
@@ -8,33 +8,33 @@
 run("/home/drtea/Research/Projects/CO2policy/PEDAC/scripts/Script_ColorDatabase.m")
 % this will produce matrices with RGB values for nice colors in the figures
 
-%%%% Analysis of AR5 models
+%%%% Preparing the data and getting fitting parameters
 % 1)
-run("/home/drtea/Research/Projects/CO2policy/PEDAC/scripts/Script_FitRafelskiModel.m")
+run("/home/drtea/Research/Projects/CO2policy/PEDAC/scripts/Script_FitJoosModel.m")
 % this run will load the historical emission and atmospheric CO2
-% observations and will use the Rafelski model to fit this data.
+% observations and will use the Joos model to fit this data.
 % It outputs the optimized paramters using least squares, processed
-% observations and atmospheric CO2 predictions
+% observations of emissions and atmospheric CO2 predictions
 
 % 2)
 run("/home/drtea/Research/Projects/CO2policy/PEDAC/scripts/Script_ErrorProcessAnalysis.m")
-% this will analyze the error process, i.e. residuals of the Rafelski fit
+% this will analyze the error process, i.e. residuals of the Joos fit
 % and output the values for AR1 process. Later this part will be outsourced
 % to R
 
 % 3)
-run("/home/drtea/Research/Projects/CO2policy/PEDAC/scripts/Script_InterpolateMonthlyAR5emissions.m")
-% this script reads the different AR5 2° and BAU scenarios and produces
-% monthly interpolated versions of it
+run("/home/drtea/Research/Projects/CO2policy/PEDAC/scripts/Script_IIASA_emissions.m")
+% this script reads the different emission scenarios and returns a .mat file
+% containing the monthly interpolated scenarios
 
 % 4)
-run("/home/drtea/Research/Projects/CO2policy/PEDAC/scripts/Script_PredictionAtmosphericCO2_RafelskiModel.m")
+run("/home/drtea/Research/Projects/CO2policy/PEDAC/scripts/Script_IIASA_ProjectionAtmosphericCO2_JoosModel.m")
 % this script uses the optimal parameters for the Rafelski model and will
 % produce predicitions of the atmospheric CO2 from it for different AR5
 % scenarios
 
 % 5)
-run("/home/drtea/Research/Projects/CO2policy/PEDAC/scripts/Script_DetectionTimeEstimates_AR5.m")
+run("/home/drtea/Research/Projects/CO2policy/PEDAC/scripts/Script_IIASA_Detection.m")
 % this script applies the detection time protocoll to the paired AR5
 % scenarios and outputs the detection time and some statistics
 

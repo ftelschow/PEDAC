6c2368ec3482acc5b9ccdee8297b66aaa5c36143
Fabian Telschow <ftelschow@ucsd.edu>
2020-01-20 11:02:22 +0100

    added directory information for server

--- scripts/Script_IIASA_ProjectionAtmosphericCO2_JoosModel.m
+++ scripts/Script_IIASA_ProjectionAtmosphericCO2_JoosModel.m
@@ -11,12 +11,21 @@
 clear all
 close all
 
+server = 1;
 % set correct working directory
-path      = '/home/drtea/Research/Projects/CO2policy/PEDAC';
-path_pics = '/home/drtea/Research/Projects/CO2policy/pics/';
-path_data = '/home/drtea/Research/Projects/CO2policy/PEDAC/data/';
-cd(path)
-clear path
+if ~server
+    path      = '/home/drtea/Research/Projects/CO2policy/PEDAC';
+    path_pics = '/home/drtea/Research/Projects/CO2policy/pics/';
+    path_data = '/home/drtea/Research/Projects/CO2policy/PEDAC/data/';
+    cd(path)
+    clear path
+else
+    path      = '~/projects/PEDAC';
+    path_pics = '~/projects/PEDAC/pics/';
+    path_data = '~/projects/PEDAC/data/';
+    cd(path)
+    clear path
+end
 
 % convert C to CO2
 C2CO2       = 44.01 / 12.011;

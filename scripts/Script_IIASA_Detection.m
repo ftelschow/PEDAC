e26869c6df3a896a09c48b8431191024699707a0
Fabian Telschow <ftelschow@ucsd.edu>
2020-01-21 12:00:47 +0100

    changed to run on the server

--- scripts/Script_IIASA_Detection.m
+++ scripts/Script_IIASA_Detection.m
@@ -86,54 +86,54 @@ for test_start = baseVec
                 detect_year( 1 : mT, scn ) = cdf_dyears;
                 thresholds_year( scn ) = thresh;
 
-                plot_Detection_cdf( [ false_detect; cdf_dyears ], test_start,...
-                                    [0.05, 0.5, 0.95 ], q,...
-                                    strcat( path_pics, 'detect_',...
-                                        method, '/Sc_', num2str(scn),...
-                                        '_detection_aCO2_IISA_base', num2str(test_start), '_',...
-                                        method, '.png' ) );
-
-                dyear = get_Quants( detect_year( 1 : mT, scn ), [ 0.05 0.5, 0.95 ] );
-
-                figure( 2 ), clf, hold on
-                WidthFig  = 500 * 1.1;
-                HeightFig = 400 * 1.1;
-                set( gcf, 'Position', [ 300 300 WidthFig HeightFig ] );
-                set( gcf, 'PaperPosition', [ 300 300 WidthFig HeightFig ] )
-                set( groot, 'defaultAxesTickLabelInterpreter', 'latex' );
-                set( groot, 'defaultLegendInterpreter', 'latex' );
-                        plot( years, drift_base, 'LineWidth', 1.5, 'Color',...
-                              ColScheme( 1, : ) )
-                        plot( years, drift_alter, 'LineWidth', 1.5, 'Color',...
-                              ColScheme( 3, : ) )
-                        plot( [ years( dyear(1) ) years( dyear(1) ) ], [ -2000, 2000 ],...
-                              'k--' )
-                        plot( [ years( dyear(2) ) years( dyear(2) ) ], [ -2000, 2000 ],...
-                              'k-', 'LineWidth', 1.5 )
-                        plot( [ years( dyear(3) ) years( dyear(3) ) ], [ -2000, 2000 ],...
-                              'k--' )
-                        title( 'atmospheric CO2' )
-                h = xlabel( 'years' );  set( h, 'Interpreter', 'latex' );
-                h = ylabel( 'atmospheric CO2 [ppm/year]' );
-                set( h, 'Interpreter', 'latex' );
-                ylim( [ 350 550 ] )
-                xlim( [ 2000 2050 ] )
-                h = legend( namesBAU{ corBAU( scn ) },...
-                            namesAlt{ scn },...
-                            'location', 'northwest' );
-                set( h, 'Interpreter', 'latex' );
-                grid
-                set( gca, 'fontsize', 14 );
-
-                set( gcf, 'papersize', [ 12 12 ] )
-                fig = gcf;
-                fig.PaperPositionMode = 'auto';
-                fig_pos = fig.PaperPosition;
-                fig.PaperSize = [ fig_pos( 3 ) fig_pos( 4 ) ];
-                print( strcat( path_pics, 'detect_', method, '/Sc_', num2str( scn ),...
-                       'DetectionTimes_aCO2_IISA_base', num2str(test_start), '_',...
-                       method, '.png' ), '-dpng' )
-                hold off
+%                 plot_Detection_cdf( [ false_detect; cdf_dyears ], test_start,...
+%                                     [0.05, 0.5, 0.95 ], q,...
+%                                     strcat( path_pics, 'detect_',...
+%                                         method, '/Sc_', num2str(scn),...
+%                                         '_detection_aCO2_IISA_base', num2str(test_start), '_',...
+%                                         method, '.png' ) );
+% 
+%                 dyear = get_Quants( detect_year( 1 : mT, scn ), [ 0.05 0.5, 0.95 ] );
+% 
+%                 figure( 2 ), clf, hold on
+%                 WidthFig  = 500 * 1.1;
+%                 HeightFig = 400 * 1.1;
+%                 set( gcf, 'Position', [ 300 300 WidthFig HeightFig ] );
+%                 set( gcf, 'PaperPosition', [ 300 300 WidthFig HeightFig ] )
+%                 set( groot, 'defaultAxesTickLabelInterpreter', 'latex' );
+%                 set( groot, 'defaultLegendInterpreter', 'latex' );
+%                         plot( years, drift_base, 'LineWidth', 1.5, 'Color',...
+%                               ColScheme( 1, : ) )
+%                         plot( years, drift_alter, 'LineWidth', 1.5, 'Color',...
+%                               ColScheme( 3, : ) )
+%                         plot( [ years( dyear(1) ) years( dyear(1) ) ], [ -2000, 2000 ],...
+%                               'k--' )
+%                         plot( [ years( dyear(2) ) years( dyear(2) ) ], [ -2000, 2000 ],...
+%                               'k-', 'LineWidth', 1.5 )
+%                         plot( [ years( dyear(3) ) years( dyear(3) ) ], [ -2000, 2000 ],...
+%                               'k--' )
+%                         title( 'atmospheric CO2' )
+%                 h = xlabel( 'years' );  set( h, 'Interpreter', 'latex' );
+%                 h = ylabel( 'atmospheric CO2 [ppm/year]' );
+%                 set( h, 'Interpreter', 'latex' );
+%                 ylim( [ 350 550 ] )
+%                 xlim( [ 2000 2050 ] )
+%                 h = legend( namesBAU{ corBAU( scn ) },...
+%                             namesAlt{ scn },...
+%                             'location', 'northwest' );
+%                 set( h, 'Interpreter', 'latex' );
+%                 grid
+%                 set( gca, 'fontsize', 14 );
+% 
+%                 set( gcf, 'papersize', [ 12 12 ] )
+%                 fig = gcf;
+%                 fig.PaperPositionMode = 'auto';
+%                 fig_pos = fig.PaperPosition;
+%                 fig.PaperSize = [ fig_pos( 3 ) fig_pos( 4 ) ];
+%                 print( strcat( path_pics, 'detect_', method, '/Sc_', num2str( scn ),...
+%                        'DetectionTimes_aCO2_IISA_base', num2str(test_start), '_',...
+%                        method, '.png' ), '-dpng' )
+%                 hold off
 
         end
 
